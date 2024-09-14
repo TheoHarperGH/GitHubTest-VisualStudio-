0683f856881f651862fa1c3e1c0b8b2c2bd3f8cb
theoj <theoj@12600KF_BUILD>
2024-09-14 18:47:12 +0100

    Add project files.

---
+++ GitHubTest/Program.cs
@@ -0,0 +1,20 @@
+﻿//Testing Visual Studio and GitHub In conjunction
+
+// odd or even
+
+
+Console.WriteLine("Choose a number: ");
+int UserInput = Convert.ToInt32(Console.ReadLine());
+
+if (UserInput % 2 == 0)
+{
+    Console.WriteLine("You entered an even number");
+}
+else
+{
+    Console.WriteLine("You entered an odd number");
+}
+
+
+
+
